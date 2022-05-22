94a7c0a4bfeeb116072c6577709dbbe16f4c5ab1
Gerrnperl <64595686+Gerrnperl@users.noreply.github.com>
2022-05-22 00:56:23 +0000

    M

--- components/script/player/meta-data.js
+++ components/script/player/meta-data.js
@@ -2,8 +2,8 @@ class MetaData extends HTMLElement {
 	constructor() {
 		// Always call super first in constructor
 		super();
-		this.attachShadow({mode: 'open'})
-		this.shadowRoot.innerHTML = document.querySelector('#template-meta-data').content;
+		this.attachShadow({mode: 'open'});
+		this.shadowRoot.appendChild(document.querySelector('#template-meta-data').content);
 	  }
 }
 customElements.define('cs-meta-data', MetaData);
--- components/script/player/player.js
+++ components/script/player/player.js
@@ -2,8 +2,8 @@ class Player extends HTMLElement {
 	constructor() {
 		// Always call super first in constructor
 		super();
-		this.attachShadow({mode: 'open'})
-		this.shadowRoot.innerHTML = document.querySelector('#template-player').content;
+		this.attachShadow({mode: 'open'});
+		this.shadowRoot.appendChild(document.querySelector('#template-player').content);
 	  }
 }
 customElements.define('cs-player', Player);
