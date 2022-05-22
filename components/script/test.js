a25337cb75eeb6c790ba3b263b9f51ac3bd5898b
Gerrnperl <64595686+Gerrnperl@users.noreply.github.com>
2022-05-22 03:09:44 +0000

    M

--- components/script/test.js
+++ components/script/test.js
@@ -1,7 +1,7 @@
 document.dispatchEvent(new CustomEvent('lx-meta-data-update',
 	{
 		'detail':{
-			songName: 'Blues', artistList: ['Cordie', 'Shaniya'], coverURL: 'http://placeimg.com/640/480',
+			songName: 'Blues', artistList: ['Cordie', 'Shaniya'], coverURL: 'https://random.imagecdn.app/100/100',
 		},
 	}
 ));
