35d3916571ffd044abac6c3e725a5beced7d5f99
Gerrnperl <64595686+Gerrnperl@users.noreply.github.com>
2022-05-22 00:52:27 +0000

    M

--- components/script/player/meta-data.js
+++ components/script/player/meta-data.js
@@ -2,7 +2,8 @@ class MetaData extends HTMLElement {
 	constructor() {
 		// Always call super first in constructor
 		super();
-		this.querySelector('.song-name').innerHTML = 'Songname';
+		this.attachShadow({mode: 'open'})
+		this.shadowRoot.innerHTML = document.querySelector('#template-meta-data').content;
 	  }
 }
 customElements.define('cs-meta-data', MetaData);
--- components/script/player/player.js
+++ components/script/player/player.js
@@ -2,9 +2,8 @@ class Player extends HTMLElement {
 	constructor() {
 		// Always call super first in constructor
 		super();
-		this.attachShadow({mode: 'open'}).appendChild(
-			document.createElement('cs-meta-data')
-		  );
+		this.attachShadow({mode: 'open'})
+		this.shadowRoot.innerHTML = document.querySelector('#template-player').content;
 	  }
 }
 customElements.define('cs-player', Player);
