a77c6152ba4aae3fdce52b862a9b70a124ac583f
Gerrnperl <64595686+Gerrnperl@users.noreply.github.com>
2022-05-22 00:28:58 +0000

    M

--- components/script/player/player.js
+++ components/script/player/player.js
@@ -2,6 +2,7 @@ class Player extends HTMLElement {
 	constructor() {
 		// Always call super first in constructor
 		super();
+		this.appendChild(document.createElement('div'));
 	  }
 }
 customElements.define('cs-player', Player);
