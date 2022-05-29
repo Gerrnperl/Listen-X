6cd847120f7b3d11b6e6d46a7fd4afbf3fd6a4f5
Gerrnperl Lunn <gerrnperl@outlook.com>
2022-05-29 13:15:21 +0800

    Init progress-ctrl

---
+++ components/script/player/progress.js
@@ -0,0 +1,10 @@
+class ProgressCtrl extends HTMLElement{
+
+	constructor(){
+		super();
+		this.attachShadow({mode: 'open'});
+		this.shadowRoot.appendChild(document.querySelector('#template-progress').content);
+	}
+
+}
+customElements.define('lx-progress-ctrl', ProgressCtrl);
